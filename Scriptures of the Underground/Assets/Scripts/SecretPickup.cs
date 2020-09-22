f956d188b3e3ef439955fd4a99cd3cc7a58526fb
Unknown <jeremy.koskamp@live.nl>
2020-09-22 14:48:27 +0200

    Monday -workday blurk day collectible system fix day

---
+++ Scriptures of the Underground/Assets/Scripts/SecretPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretPickup : MonoBehaviour
+{
+    public InventorySecrets inventorysystem;
+    public Item SecretObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            AddItems();
+        }
+    }
+
+    public void AddItems()
+    {
+        inventorysystem.Add(SecretObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Scriptures of the Underground/Assets/Scripts/UI/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Secret", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+
+    new public string name = "New item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+    [TextArea]
+    public string info;
+
+    public virtual void Use()
+    {
+        //use the item
+        //thing happens
+        Debug.Log("Using" + name);
+        InventorySecrets.instance.infoHolder.SetActive(true);
+        InventorySecrets.instance.ChangeInfoText(info);
+    }
+}
