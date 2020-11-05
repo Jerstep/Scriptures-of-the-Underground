b148ba110fde4a039216a458ab234c7e19590aa1
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2020-11-05 13:57:12 +0100

    collecting thingys

---
+++ Scriptures of the Underground/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+
+    int collectiblesFound;
+    int alertedEnemies;
+    int deaths;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    public void EndDemo(int collectibleAmount)
+    {
+        collectiblesFound = collectibleAmount;
+    }
+}
