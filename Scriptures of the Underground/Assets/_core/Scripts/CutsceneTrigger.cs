1461187b0553b255e899935bcd9b651f1c5c375a
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2020-12-15 12:47:49 +0100

    fmody things

--- Scriptures of the Underground/Assets/_core/Scripts/CutsceneTrigger.cs
+++ Scriptures of the Underground/Assets/_core/Scripts/CutsceneTrigger.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using Cinemachine.Editor;
 namespace SA
 {
     public class CutsceneTrigger : MonoBehaviour
--- Scriptures of the Underground/Assets/_core/Scripts/Gamemanager.cs
+++ Scriptures of the Underground/Assets/_core/Scripts/Gamemanager.cs
@@ -16,7 +16,13 @@ public class Gamemanager : MonoBehaviour
         
     }
 
-    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 
     public void EndDemo(int collectibleAmount)
     {
