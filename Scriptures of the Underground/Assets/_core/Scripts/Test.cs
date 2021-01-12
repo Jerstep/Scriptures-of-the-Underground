deb9e932f494e3095804b2c18004e4a2b8d67cdd
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2021-01-12 10:37:24 +0100

    monday prefabs

--- Scriptures of the Underground/Assets/_core/Scripts/Test.cs
+++ Scriptures of the Underground/Assets/_core/Scripts/Test.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-
+    public string audiopath;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             Debug.Log("Player IS COLLIDING!!!!");
+            CallAudio();
         }
+    }
+
+    public void CallAudio()
+    {
+        FMOD.Studio.EventInstance AudioPath = FMODUnity.RuntimeManager.CreateInstance(audiopath);
+        AudioPath.start();
+        AudioPath.release();
     }
 
     /*private static bool created = false;
